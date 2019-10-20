c16b633784d62fc82005e77e671794277efa7b4f
stubidoo <stuart.glass2@gmail.com>
2019-10-20 11:27:24 +0100

    added comments and more

--- index.js
+++ index.js
@@ -4,4 +4,6 @@
 // sort - Alphabetise a list
 
 // Making a posting schedual
-
+// - Video about different setups vscode node etc
+// - Video about difference between node.js and browser runtime
+// - 
