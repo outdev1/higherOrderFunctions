9aae9d6b14965c6f5f838a4113266b81264b8e97
stubidoo <stuart.glass2@gmail.com>
2019-10-20 11:25:31 +0100

    added comments

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// filter
-// map
-// reduce
-// sort
+// filter - range of nubers
+// map - transform a user obkect
+// reduce - research this one
+// sort - Alphabetise a list
+
+// Making a posting schedual
 
